0bd16e92768a350988654a0d210ed3e8279d17b3
Riccardo <riccardo_london@live.it>
2019-03-16 23:59:34 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -10,7 +10,7 @@ class Anagram
   def match(checker)
     returner = []
    checker.each { |word| word.chars.sort!.join}
-   self.chars.sort!.join
+   self.name.chars.sort!.join
    i = checker.length
    checkerrr = []
    while (i > 0)
